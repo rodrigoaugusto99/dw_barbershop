bcb4423417d06580a573173aa0d284263f1ac598
rodrigoaugusto99 <rodrigoaugusto51@outlook.com>
2023-09-04 20:48:21 -0300

    auth_interceptor e globalKey p navegação sem contexto

--- lib/src/core/restClient/auth_interceptor.dart
+++ lib/src/core/restClient/auth_interceptor.dart
@@ -26,6 +26,23 @@ class AuthInterceptor extends Interceptor {
     handler.next(options);
   }
 
+/*Ciclo de vida da requisicao http, passa pelo onRequest que é a 
+pré requisicao, na volta pode ter o onRespose, que é o sucesso, 
+e o onError q é uma volta com erro.
+onResponse podemos implementar aqui no interceptor para caso a gente 
+queira fazer alguma coisa antes de responder pra quem me chamou.
+
+foi feito o onRequest p adicionar o token nas requisiçoes.requisicoes autenticaas
+
+mas e se o token expirar? vai dar error 403 forbidden
+podemos fazer um refresh no token, ou pedir pro usuario logar de novo
+
+Vamos pedir p usuario logar de novo!
+
+Para mandar o usuario para a tela de login, nãot emos um context
+para fazer o redirect, entao teremos que fazer com base numa globalKey
+-global key será feito em outro arquivo com o conceito de singletown
+terá uma unica instancia por toda a aplicacao*/
   @override
   void onError(DioException err, ErrorInterceptorHandler handler) {
     final DioException(requestOptions: RequestOptions(:extra), :response) = err;
--- lib/src/core/ui/barbershop_nav_global_key.dart
+++ lib/src/core/ui/barbershop_nav_global_key.dart
@@ -3,8 +3,12 @@ import 'package:flutter/material.dart';
 class BarbershopNavGlobalKey {
   static BarbershopNavGlobalKey? _instance;
   // Avoid self instance
+  //global key do tipo NavigatorState!
   final navKey = GlobalKey<NavigatorState>();
   BarbershopNavGlobalKey._();
   static BarbershopNavGlobalKey get instance =>
       _instance ??= BarbershopNavGlobalKey._();
 }
+/*contexto estatico e metodo estatico de instante, e tudo
+que eu guardar dentro dessa instancia, vai ser estatico,
+uma unica instancioa*/
